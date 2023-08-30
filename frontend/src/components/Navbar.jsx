41f0069de64f3ca985c95d42efe2431e89cdf5e5
koussay-r <koussayr98@gmail.com>
2023-08-30 15:34:35 +0100

    working on the menu button animation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiSearch} from "react-icons/bi"
 import {BsTelephoneFill} from "react-icons/bs"
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className='flex bg-[#020230] justify-between p-4'>
         <p className='text-[26px] font-bold text-white'>
@@ -22,7 +27,16 @@ export default function Navbar() {
            <p className='text-white'>Call : +01 123455678990 </p> 
             </div>
         </div>
-        
+        <button lassName={`menu ${isMenuOpen ? 'opened' : ''}`}
+      onClick={toggleMenu}
+      aria-expanded={isMenuOpen}
+      aria-label="Main Menu">
+      <svg width="100" height="100" viewBox="0 0 100 100">
+        <path classNAme={"line line1"} d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+        <path classNAme={"line line2"} d="M 20,50 H 80" />
+        <path classNAme={"line line3"} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+      </svg>
+    </button>
     </div>
   )
 }
