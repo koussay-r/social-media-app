9845f82a19cc4510f68b19d3ae015e8e5b763467
koussay-r <koussayr98@gmail.com>
2023-09-05 16:38:36 +0100

    done with the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import slider from "./../assets/slider-img.png"
 export default function Home() {
   return (
-    <div className='bg-[#020230] h-[100hv] pt-16 gap-24 lg:justify-center block mx-auto lg:flex'>
-        <div className='lg:w-[38%] block mx-auto  lg:mt-[100px]'>
-            <p className=' text-center  text-white font-oswald text-4xl font-bold'>Fast & Secure<br/>
+    <div className='bg-[#020230] lg:pb-[155px] h-[100hv] pt-16   lg:justify-center block mx-auto lg:flex'>
+        <div className='lg:w-[30%] sm:w-[70%]  block mx-auto  lg:mt-[100px]'>
+            <p className=' text-center lg:text-start  text-white font-oswald text-4xl font-bold'>Fast & Secure<br/>
 Web Hosting </p>
-<p className=' text-center px-1 text-white font-semibold mt-5'> Anything embarrassing hidden in the middle of text. All the Lorem Ipsuanything embarrassing hidden in the middle of text. All the Lorem Ipsumm
+<p className=' text-center lg:text-start px-1  text-white font-semibold mt-5'> Anything embarrassing hidden in the middle of text. All the Lorem Ipsuanything embarrassing hidden in the middle of text. All the Lorem Ipsumm
 </p>
 <div className='flex justify-center lg:justify-normal mt-5 gap-5'>
     <button className='lg:text-[15px] text-[12px] transition-all duration-300 hover:text-[#03a7d3] hover:bg-transparent  bg-[#03a7d3] rounded px-12 lg:px-16 font-semibold py-3 border border-[#03a7d3] text-white'>Read More</button>
     <button className='lg:text-[15px] text-[12px] transition-all duration-300 hover:text-[#ff4646] hover:bg-transparent  bg-[#ff4646] rounded px-12 lg:px-16 font-semibold py-3 border border-[#ff4646] text-white'>Contact Us</button>
 </div>
         </div>
-        <img src={slider} className='lg:w-[440px] mt-10 lg:mt-0  h-[390px] w-[390px] lg:h-[440px]'/>
+        <img src={slider} className='lg:w-[440px] block mx-auto mt-10 lg:mt-0  h-[390px] w-[390px] lg:h-[440px]'/>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   };
   return (
     <div className='flex bg-[#020230] justify-between p-4'>
-        <p className='text-[26px] font-bold text-white'>
+        <p className='text-[19px] lg:text-[26px] mt-2 lg:mt-0 font-bold text-white'>
             HOSTIT
         </p>
         <div className=' hidden lg:flex pt-2 gap-10'>
@@ -27,13 +27,32 @@ export default function Navbar() {
            <p className='text-white'>Call : +01 123455678990 </p> 
             </div>
         </div>
+        <div className={`transition-all overflow-auto lg:hidden pr-5 overflow-y-hidden mt-[60px]  block mx-auto  duration-500 ${isMenuOpen? "h-[380px]":"h-0"}`}>
+      <div className='z-[-1]'>
+      <ul className='flex flex-col-reverse text-white gap-8'>
+                <li><div className='flex justify-center text-white gap-2'>
+
+<BsTelephoneFill className='text-white mt-1'/>
+<p className='text-white'>Call : +01 123455678990 </p> 
+</div></li>
+                <li>
+                <BiSearch color='white' size={"22"} className='mt-1 block mx-auto cursor-pointer'/>
+                </li>
+                <li className=' font-semibold text-center cursor-pointer'>CONTACT US</li>
+                <li className=' font-semibold text-center cursor-pointer'>PRICING</li>
+                <li className=' font-semibold text-center cursor-pointer'>SERVICE</li>
+                <li className=' font-semibold text-center cursor-pointer'>ABOUT</li>
+                <li className=' font-semibold text-center cursor-pointer'>HOME</li>
+            </ul>
+      </div>
+    </div>
         <button
       className={`menu block lg:hidden ${isMenuOpen ? 'opened' : ''}`}
       onClick={toggleMenu}
       aria-expanded={isMenuOpen}
       aria-label="Main Menu"
     >
-      <svg width="50" height="50"  viewBox="0 0 100 100">
+      <svg className='w-[40px] mt-1 lg:mt-0 lg:h-[50px] lg:w-[50px] h-[40px]'  viewBox="0 0 100 100">
         <path
           className={`line  line1 ${isMenuOpen ? 'opened' : ''}`}
           d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
@@ -45,17 +64,7 @@ export default function Navbar() {
         />
       </svg>
     </button>
-    <div className={`transition-all duration-1000 ${isMenuOpen? "h-[300px]":"h-0"}`}>
-      <div>
-      <ul className='flex flex-col-reverse text-white gap-16'>
-                <li className=' text-center cursor-pointer'>HOME</li>
-                <li className=' text-center cursor-pointer'>ABOUT</li>
-                <li className=' text-center cursor-pointer'>SERVICE</li>
-                <li className=' text-center cursor-pointer'>PRICING</li>
-                <li className=' text-center cursor-pointer'>CONTACT US</li>
-            </ul>
-      </div>
-    </div>
+    
     </div>
   )
 }
