38ab9c79811af811ec1a4b5d4f1ddd1637c6d355
koussay-r <koussayr98@gmail.com>
2023-12-30 17:20:31 +0100

    update

--- frontend/src/components/navbar/menuButton.jsx
+++ frontend/src/components/navbar/menuButton.jsx
@@ -23,6 +23,7 @@ export default function AccountMenu() {
     dispatch(changeAuth())
     localStorage.removeItem("account")
     sessionStorage.removeItem("account")
+    sessionStorage.removeItem("profilePic")
     document.body.style.backgroundColor="#f3f3f3"
     navigate("/")
     window.location.reload();
